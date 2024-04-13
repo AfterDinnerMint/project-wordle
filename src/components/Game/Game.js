6032f319a2393eaac1502474fd69b0d34f04eb1f
AfterDinnerMint <87070041+AfterDinnerMint@users.noreply.github.com>
2024-04-13 07:05:32 +0000

    Complete fifth task

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -13,8 +13,19 @@ console.info({ answer });
 function Game() {
   const [guesses, setGuesses] = React.useState([]);
 
+  const gameWon = guesses.includes(answer);
+  const gameLost = !gameWon && guesses.length === 6;
+
   return <>
     <GuessResults answer={answer} guesses={guesses} />
+    {gameWon && <div class="happy banner">
+      <p>
+        <strong>Congratulations!</strong> Got it in <strong>{guesses.length} guesses</strong>.
+      </p>
+    </div>}
+    {gameLost && <div class="sad banner">
+      <p>Sorry, the correct answer is <strong>{answer}</strong>.</p>
+    </div>}
     <GuessInput onGuess={(guess) => setGuesses([...guesses, guess])} />
   </>;
 }
